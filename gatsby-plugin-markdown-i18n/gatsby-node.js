f6df9ddfa14bf42839c8d39f239e4c5f73ebcf4a
miltonbolonha <miltonbolonha@gmail.com>
2023-02-04 20:54:08 -0300

    updated

--- gatsby-plugin-markdown-i18n/gatsby-node.js
+++ gatsby-plugin-markdown-i18n/gatsby-node.js
@@ -1,9 +1,3 @@
-// import { i18n } from "@lingui/core";
-// import { I18nProvider } from "@lingui/react";
-// import { ThemeProvider } from "./src/context/ThemeContext";
-// const ptMessages = require("../content/i18n/pt-BR/main");
-// import { messages as enMessages } from "../content/i18n/en-US/main";
-// const i18nConfig = require("../.linguirc");
 const path = require("path");
 const rootDir = path.join(__dirname, "../");
 const businessInfos = require("../content/configs/schema-org.json");
@@ -23,8 +17,6 @@ function basePathFinder(nodeTopology) {
   return null;
 }
 // Adding slug field to each post
-// exports.= async ({
-
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
 
--- gatsby-plugin-sitepages-i18n/gatsby-node.js
+++ gatsby-plugin-sitepages-i18n/gatsby-node.js
@@ -8,15 +8,6 @@ const schemaOrg = require(path.resolve(
   rootDir,
   `content/configs/schema-org.json`
 ));
-const schemaOrgEN = require(path.resolve(
-  rootDir,
-  `content/configs/schema-org.en.json`
-));
-const reqSchemaDefault = require(path.resolve(
-  rootDir,
-  "content/schemas/default.json"
-));
-
 // i18n default card
 const card = schemaOrg.schema[0].card[0];
 // i18n array locales ['xx-XX','xx-XX']
@@ -25,12 +16,9 @@ const locales = schemaOrg.locales;
 //require theme pagesSite
 const pageSiteFolder = path.resolve(rootDir, `${card.themePath}/src/pages`);
 
-// const cardEN = schemaOrgEN.schema[0].card[0];
-// const filteredCard = null;
-
 // create Node Field in SitePage node type
 exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNode, createNodeField, createNodeId } = actions;
+  const { createNodeField } = actions;
   if (node.internal.type === "SitePage") {
     const x = [
       {
@@ -55,9 +43,8 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 
 // createPages i18n from SitePages
 exports.createPages = async function ({ graphql, actions, page, reporter }) {
-  const { createPage, deletePage, createRedirect } = actions;
+  const { createPage } = actions;
   // first array locale position it's reserved to default locale
-  const defaultLocale = locales[0];
 
   const pageSiteObj = (file, schema) => {
     const x = file.split(".")[0];
@@ -75,47 +62,31 @@ exports.createPages = async function ({ graphql, actions, page, reporter }) {
     pageSitename,
     schemaJSON
   ) => {
-    // console.log("iniciar create pages i18n index 404's and plus");
     // index
     // 404
     // 404.html
-    for (let index = 0; index < schemaJSON.length; index++) {
-      let element = schemaJSON[index];
-
-      const isDefaultI18n = schemaJSON[index] === "default.json" ? true : false;
+    for (const element of schemaJSON) {
+      const isDefaultI18n = element === "default.json" ? true : false;
       const isIndex = pageSitename === "index.js" ? true : false;
       const is404 = pageSitename === "404.js" ? true : false;
-      const localePathQuery = isDefaultI18n
-        ? ""
-        : schemaJSON[index].slice(0, 2);
+      const localePathQuery = isDefaultI18n ? "" : element.slice(0, 2);
 
       const pathQuery = isIndex ? "" : pageSitename;
       const pathExtended =
-        schemaJSON[index] === "default.json"
+        element === "default.json"
           ? "/" + localePathQuery
           : "/" + localePathQuery + "/" + pathQuery;
       pageSiteObj.path = pathExtended;
-      // pageSiteObj.context.schemaJSON = require(path.resolve(
-      //   rootDir,
-      //   `content/schemas/${element}`
-      // ));
 
       //if index 404 404.html
       // await createPage(pageSiteObj);
       if (is404) {
-        console.log("is 404 page");
-        console.log(pageSiteObj);
         await createPage(pageSiteObj);
       }
       if (isDefaultI18n && isIndex) {
-        console.log("is index default page");
-        console.log(pageSiteObj);
         await createPage(pageSiteObj);
       } else {
-        console.log("todas as pagesSites aqui");
-        console.log(pageSiteObj);
         await createPage(pageSiteObj);
-        // console.log("i18n pagesite created");
       }
     }
   };
@@ -123,12 +94,7 @@ exports.createPages = async function ({ graphql, actions, page, reporter }) {
   // grab all files in themePath/src/pages
   const mapPageSites = async schemaJSON => {
     fs.readdir(pageSiteFolder, (err, files) => {
-      // console.log("Folder readed: pagesiteFolder files");
-      // console.log(files.split(".")[0]);
       files.map((pageSitename, ind) => {
-        // console.log("initialize create PagesSites I18n w/ ");
-        // console.log("pageSite for");
-        // console.log(pageSitename);
         createPagesSitesI18n(
           pageSiteObj(pageSitename),
           pageSitename,
@@ -140,7 +106,6 @@ exports.createPages = async function ({ graphql, actions, page, reporter }) {
 
   let x = null;
   fs.readdir(schemasPath, (err, schemasFiles) => {
-    // console.log("iniciar mapeamento e criação de sitePages com schemas ");
     mapPageSites(schemasFiles);
   });
 };
